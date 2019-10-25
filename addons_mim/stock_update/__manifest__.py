71876738b452d902bc50ca68defcf1f592829c1a
Valian-Tsiaro <valian.tsiaro@gmail.com>
2019-10-25 10:51:17 +0300

    [ADD] stock_update - Tsiaro

---
+++ addons_mim/stock_update/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "stock_update",
+
+    'summary': """
+    """,
+
+    'description': """
+
+    """,
+
+    'author': "INGENOSYA",
+    'website': "http://www.ingenosya.com",
+
+    'category': 'Uncategorized',
+    'version': '2.0',
+
+    'depends': ['base', 'stock'],
+
+    'data': [
+        'views/stock_view.xml',
+    ],
+}
