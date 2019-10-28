19ebc7d2a1e66cf2ecf0d95d76988526c4f40f53
Valian-Tsiaro <valian.tsiaro@gmail.com>
2019-10-28 10:43:27 +0300

    [FIX] mail_notification - Tsiaro

--- addons_mim/mail_notification/models/stock.py
+++ addons_mim/mail_notification/models/stock.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models
+from odoo import models, fields
 
 
 class StockPicking(models.Model):
     _name = 'stock.picking'
     #Héritage de l'objet stock.picking.out pour qu'il hérite 
     #de l'objet ir.needaction_mixin (bulles de notification de messages)
-    #  ---- remove because useless since V9 'ir.needaction_mixin'
-    _inherit = ['stock.picking']
+    #  ---- 'ir.needaction_mixin' removed and replaced with 'mail.activity.mixin'
+    _inherit = ['mail.activity.mixin', 'stock.picking']
     
