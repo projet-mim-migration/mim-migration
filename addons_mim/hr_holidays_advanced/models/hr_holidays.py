7d5053aa22735380502f16a29b454bd2a7a0a9f0
nary <narisoaandrians@gmail.com>
2019-11-15 17:42:04 +0300

    [FIX] hr_holidays_advanced verif Nary

--- addons_mim/hr_holidays_advanced/models/hr_holidays.py
+++ addons_mim/hr_holidays_advanced/models/hr_holidays.py
@@ -6,16 +6,26 @@ from odoo import netsvc
 class HrHolidays(models.Model):
     _inherit = 'hr.leave'
     
-    @api.model
-    def write(self, vals):
-        employee_id = vals.get('employee_id', False)
-        #enlever pour que le nouveau senario marche
-        #if vals.get('state') and vals['state'] not in ['draft', 'confirm', 'cancel'] and not self.pool['res.users'].has_group(cr, uid, 'base.group_hr_user'):
-            # raise osv.except_osv(_('Warning!'), _('You cannot set a leave request as \'%s\'. Contact a human resource manager.') % vals.get('state'))
-        #hr_holiday_id = super(hr_holidays, self).write(cr, uid, ids, vals, context=context)
-        hr_holiday_id = models.Model.write(self, vals)
+    @api.multi
+    def write(self, values):
+        employee_id = values.get('employee_id', False)
+        hr_holiday_id = super(HrHolidays, self).write(values)#models.Model.write(self, values)
         self.add_follower(employee_id)
         return hr_holiday_id
+
+    '''@api.model
+                def write(self, vals):
+                    print('**********************************************')
+                    print('**********************************************')
+                    print('**********************************************')
+                    employee_id = vals.get('employee_id', False)
+                    #enlever pour que le nouveau senario marche
+                    #if vals.get('state') and vals['state'] not in ['draft', 'confirm', 'cancel'] and not self.pool['res.users'].has_group(cr, uid, 'base.group_hr_user'):
+                        # raise osv.except_osv(_('Warning!'), _('You cannot set a leave request as \'%s\'. Contact a human resource manager.') % vals.get('state'))
+                    #hr_holiday_id = super(hr_holidays, self).write(cr, uid, ids, vals, context=context)
+                    hr_holiday_id = models.Model.write(self, vals)
+                    self.add_follower(employee_id)
+                    return hr_holiday_id'''
 
     @api.multi    
     def test_manager(self):
@@ -28,7 +38,8 @@ class HrHolidays(models.Model):
                 print("COACH\nCOACH\nCOACH\nCOACH\nCOACH\n {}".format(manager_id))
             user_id = manager_id.user_id.id
             if self.env.user.id in (user_id,):
-                netsvc.LocalService("workflow").trg_validate('hr.leave', holiday.id, 'validate')
+                self.env['hr.leave'].search([('id','=',self.ids[0])]).write({'state':'validate'})
+                #netsvc.LocalService("workflow").trg_validate('hr.leave', holiday.id, 'validate')
             else:
                 print("USER\nUSER\nUSER\n {}".format(self.env.user.id)) 
                 raise exceptions.UserError((u"Seul le Résponsable hiérarchique %s a le droit d'approuver la demande de congé de  %s" \
